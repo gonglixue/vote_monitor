9b9fe657fea1b2a5961c6458a41ad18052f99263
gonglixue <724351282@qq.com>
2018-07-23 19:46:38 +0800

    add server interface

--- server/routes.py
+++ server/routes.py
@@ -25,28 +25,66 @@ def response_last_minuts():
     # print(request.args['minutes'], request.args['singer'])
     name = request.args['singer']
     minutes = int(request.args['minutes'])
-    # query = json.loads(request.get_data())
-    # minutes = query["minutes"]
-    # print(minutes)
-    # name = request.args[]
+
     query_results = server_db.get_per_minute_vote(singer_name=name, minutes=minutes)
 
     if len(query_results) > 0:
         response = Response(json.dumps({"ok":1, "data":query_results, "message":"ok"}))
     else:
-        response = Response(json.dumps({"ok":1, "data":[], "message":"not a valid query"}))
+        response = Response(json.dumps({"ok":-1, "data":[], "message":"not a valid query"}))
+        # todo: log
 
 
     response.status_code = 200
     response.headers['Content-Type'] = 'text/json'
     return response
 
+@app.route('/last_hours', methods=['GET'])
+def response_last_hours():
+    name = request.args['singer']
+    hours = int(request.args['hours'])
+
+    query_results = server_db.get_per_hour_vote(singer_name=name, hours=hours)
+
+    if len(query_results) > 0:
+        response = Response(json.dumps({"ok":1, "data":query_results, "message":"ok"}))
+    else:
+        response = Response(json.dumps({"ok":-1, "data":[], "message": "not a valid query"}))
+        # todo: log
+
+    response.status_code = 200
+    response.headers['Content-Type'] = 'text/json'
+    return response
+
+
+@app.route('/per_gap_in_last_hours', methods=['GET'])
+def response_per_gap_in_last_hours():
+    name = request.args['singer']
+    last_hours = int(request.args['last_hours'])
+    gap_minutes = int(request.args['gap_minutes'])
+
+    query_results = server_db.get_vote_given_gap(singer_name=name, gap_minutes=gap_minutes, in_last_hours=last_hours)
+
+    if len(query_results) > 0:
+        response = Response(json.dumps({"ok":1, "data":query_results, "message":"ok"}))
+    else:
+        response = Response(json.dumps({"ok":1, "data":query_results, "message":"not a valid query"}))
+
+    response.status_code = 200
+    response.headers['Content-Type'] = 'text/json'
+    return response
+
+
 @app.route('/real_time', methods=['GET'])
 def response_real_time():
     name_list = ['吴宣仪', '孟美岐', 'Yamy', '周洁琼', '鞠婧祎', '李艺彤', '于文文', '陈意涵', '王菊', '苏诗丁']
     query_results = server_db.get_latest_vote(name_list)
 
-    response = Response(json.dumps({"ok":1, "data":query_results}))
+    if len(query_results) > 0:
+        response = Response(json.dumps({"ok":1, "data":query_results}))
+    else:
+        response = Response(json.dumps({"ok":-1, "data":[], "message": "not a valid query"}))
+
     response.status_code = 200
     response.headers['Content-Type'] = 'text/json'
     return response
