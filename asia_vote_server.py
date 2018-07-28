70cc24dd78e286a4b7525e8fe5da57b5b428fa76
gonglixue <724351282@qq.com>
2018-07-28 21:10:11 +0800

    production host: inner-net ip

--- asia_vote_server.py
+++ asia_vote_server.py
@@ -1,10 +1,8 @@
 from server import app
 import sys
 
-'''
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        app.run(port=sys.argv[1])
+        app.run(host='172.18.41.219', port=sys.argv[1])
     else:
-        app.run(port=80)
-'''
+        app.run(host='172.18.41.219', port=80)
