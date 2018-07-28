efe984deb653ec01c3357379325a705f9c49b41f
gonglixue <724351282@qq.com>
2018-07-28 20:33:01 +0800

    port command

--- asia_vote_server.py
+++ asia_vote_server.py
@@ -1,4 +1,8 @@
 from server import app
+import sys
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    if len(sys.argv) > 1:
+        app.run(port=sys.argv[1])
+    else:
+        app.run(port=80)
