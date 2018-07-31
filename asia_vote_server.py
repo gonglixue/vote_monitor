e073042fe7ea89d11c1948579c6f87b4d53c6798
gonglixue <724351282@qq.com>
2018-07-31 20:31:05 +0800

    wx xml parse

--- asia_vote_server.py
+++ asia_vote_server.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        app.run(host='172.18.41.219', port=sys.argv[1])
+        app.run(host='127.0.0.1', port=sys.argv[1])
     else:
         app.run(host='172.18.41.219', port=80)
---
+++ utils/wx_receive.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as ET
+
+def parse_xml(wx_data):
+    if len(web_data) == 0:
+        return None
+
+    xmlData = ET.fromstring(wx_data)
+    msg_type = xmlData.find('MsgType').text
+
+    if msg_type == 'text':
+        return TextMsg(xmlData)
+    elif msg_type == 'image':
+        return ImageMsg(xmlData)
+
+class WxMsg(object):
+    def __init__(self, xmlData):
+        self.ToUserName = xmlData.find('ToUserName').text
+        self.FromUserName = xmlData.find('FromUserName').text
+        self.CreateTime = xmlData.find('CreateTime').text
+        self.MsgType = xmlData.find('MsgType').text
+        self.MsgId = xmlData.find('MsgId').text
+
+class TextMsg(WxMsg):
+    def __init__(self, xmlData):
+        WxMsg.__init__(xmlData)
+        self.Content = xmlData.find('Content').text.encode('utf-8') # string content
+
+class ImageMsg(WxMsg):
+    def __init__(self, xmlData):
+        WxMsg.__init__(xmlData)
+        self.PicUrl = xmlData.find('PicUrl').text
+        self.MediaId = xmlData.find('MediaId').text
