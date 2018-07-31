2ab5eb86a85a1477326fe1c64f7a3d2da642d5aa
gonglixue <724351282@qq.com>
2018-07-31 20:34:30 +0800

    wx replay class

---
+++ utils/wx_reply.py
@@ -0,0 +1,20 @@
+import time
+
+class WxReply(object):
+    def __init__(self):
+        pass
+    def send(self):
+        return "success"
+
+class TextReply(WxReply):
+    def __init__(self):
+        pass
+    def send(self):
+        pass
+
+class ImageReply(WxReply):
+    def __init__(self):
+        pass
+    def send(self):
+        pass
+
